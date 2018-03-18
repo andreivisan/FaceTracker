63607f2579a05cb24c55c0f391069c456d000204
Andrei Visan <andrei.visan@kpn.com>
2018-03-18 13:19:39 +0100

    First commit - Finished setting up OpenCV integration

---
+++ app/src/main/java/facetracker/programminglive/io/facetracker/MainActivity.java
@@ -0,0 +1,22 @@
+package facetracker.programminglive.io.facetracker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.Toast;
+
+import org.opencv.android.OpenCVLoader;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        if(OpenCVLoader.initDebug()) {
+            Toast.makeText(getApplicationContext(), "OpenCV loaded successfully", Toast.LENGTH_SHORT).show();
+        } else {
+            Toast.makeText(getApplicationContext(), "Could not load OpenCV", Toast.LENGTH_SHORT).show();
+        }
+    }
+}
