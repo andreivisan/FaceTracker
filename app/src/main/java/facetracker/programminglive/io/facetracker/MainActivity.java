80cd0e489ba0c3cb55a8f1c02d04f8cb58785324
Andrei Visan <andrei.visan@kpn.com>
2018-03-19 18:00:30 +0100

    Started camera in OpenCV with wrong rotation

--- app/src/main/java/facetracker/programminglive/io/facetracker/MainActivity.java
+++ app/src/main/java/facetracker/programminglive/io/facetracker/MainActivity.java
@@ -2,21 +2,97 @@ package facetracker.programminglive.io.facetracker;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.SurfaceView;
 import android.widget.Toast;
 
+import org.opencv.android.BaseLoaderCallback;
+import org.opencv.android.CameraBridgeViewBase;
+import org.opencv.android.JavaCameraView;
 import org.opencv.android.OpenCVLoader;
+import org.opencv.core.Core;
+import org.opencv.core.CvType;
+import org.opencv.core.Mat;
+import org.opencv.imgproc.Imgproc;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements CameraBridgeViewBase.CvCameraViewListener2 {
+
+    CameraBridgeViewBase mCameraBridgeViewBase;
+    BaseLoaderCallback mBaseLoaderCallback;
+
+    Mat mat1, mat2, mat3;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        if(OpenCVLoader.initDebug()) {
-            Toast.makeText(getApplicationContext(), "OpenCV loaded successfully", Toast.LENGTH_SHORT).show();
+        mCameraBridgeViewBase = (JavaCameraView)findViewById(R.id.mainCameraView);
+        mCameraBridgeViewBase.setVisibility(SurfaceView.VISIBLE);
+        mCameraBridgeViewBase.setCvCameraViewListener(this);
+
+        mBaseLoaderCallback = new BaseLoaderCallback(this) {
+            @Override
+            public void onManagerConnected(int status) {
+
+                switch(status) {
+                    case BaseLoaderCallback.SUCCESS:
+                        mCameraBridgeViewBase.enableView();
+                        break;
+                    default:
+                        super.onManagerConnected(status);
+                        break;
+                }
+            }
+        };
+    }
+
+    @Override
+    public Mat onCameraFrame(CameraBridgeViewBase.CvCameraViewFrame inputFrame) {
+        mat1 = inputFrame.rgba();
+
+        return mat1;
+    }
+
+    @Override
+    public void onCameraViewStarted(int width, int height) {
+        mat1 = new Mat(width, height, CvType.CV_8UC4);
+        mat2 = new Mat(width, height, CvType.CV_8UC4);
+        mat3 = new Mat(width, height, CvType.CV_8UC4);
+
+
+    }
+
+    @Override
+    public void onCameraViewStopped() {
+        mat1.release();
+        mat2.release();
+        mat2.release();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        if(mCameraBridgeViewBase != null) {
+            mCameraBridgeViewBase.disableView();
+        }
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        if(!OpenCVLoader.initDebug()) {
+            Toast.makeText(getApplicationContext(), "There is a problem in openCV", Toast.LENGTH_SHORT).show();
         } else {
-            Toast.makeText(getApplicationContext(), "Could not load OpenCV", Toast.LENGTH_SHORT).show();
+            mBaseLoaderCallback.onManagerConnected(BaseLoaderCallback.SUCCESS);
+        }
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        if(mCameraBridgeViewBase != null) {
+            mCameraBridgeViewBase.disableView();
         }
     }
 }
